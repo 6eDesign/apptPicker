97878ccd96d812d44a47b2ad504a06d4bfb9366c
Jonathan Greenemeier <jon@6eDesign.net>
2014-07-01 07:22:25 -0600

    some stuff

---
+++ build/js/JGBind.js
@@ -0,0 +1,247 @@
+var JGBinder = (function(w,d,c,$){
+  var extend = function() {
+    for(var i=1; i<arguments.length; i++)
+      for(var key in arguments[i])
+        if(arguments[i].hasOwnProperty(key))
+          arguments[0][key] = arguments[i][key];
+    return arguments[0];
+  }
+
+  // some state-tracking mechanisms: 
+  var state = { 
+    queue:              [ ], 
+    processingQueue:    false, 
+    domCollected:       false, 
+    objects:            { }, 
+    originalObjects:    { }, 
+    boundelems:         { },
+    changeHandlers:     { }
+  }
+
+  // get all the elements and find those w/ bindings: 
+  var collectDOM = function() { 
+    state.domCollected = false; 
+    var all = document.getElementsByTagName('*'); 
+    for(var i=0; i < all.length; ++i) { 
+      if(!all[i].getAttribute('JGBound')) { 
+        var bindType = false; 
+        var str = showStr = hideStr = ''; 
+
+        // first check if it is a JGBind or JGValue binding: 
+        if(all[i].getAttribute('JGBind')) { 
+          bindType = 'bind'; 
+          str = all[i].getAttribute('JGBind');
+        } else if(all[i].getAttribute('JGValue')) { 
+          bindType = 'value'; 
+          str = all[i].getAttribute('JGValue'); 
+        } 
+        // showStr = (all[i].getAttribute('JGShow')) ? all[i].getAttribute('JGShow') : false; 
+        // hideStr = (all[i].getAttribute('JGHide')) ? all[i].getAttribute('JGHide') : false; 
+
+        if(bindType) { 
+          all[i].setAttribute('JGBound','true'); 
+          all[i].onkeyup = function() { analyzechange(this); }; 
+          str = str.split('.');  
+          var objname = str.shift(); 
+          var objkey = str.join('.'); 
+          if(typeof state.boundelems[objname] == 'undefined') { 
+            state.boundelems[objname] = []; 
+          } 
+          state.boundelems[objname].push({ 
+            el: all[i], 
+            key: objkey, 
+            type: bindType
+          }); 
+        }
+        // if(showStr) { 
+        //   showStr = showStr.split('.'); 
+        //   var objname = showStr.shift(); 
+        //   state.boundelems[objname].push({
+        //     el: all[i], 
+        //     key: showStr.join('.'), 
+        //     type: 'show' 
+        //   }); 
+        // } 
+        // if(hideStr) { 
+        //   hideStr = hideStr.split('.'); 
+        //   var objname = hideStr.shift(); 
+        //   state.boundelems[objname].push({
+        //     el: all[i], 
+        //     key: hideStr.join('.'), 
+        //     type: 'hide' 
+        //   }); 
+        // }
+      }
+    }
+    state.domCollected = true; 
+    if(state.queue.length > 0) { 
+      processQueue(); 
+    } 
+  }; 
+
+  var analyzechange = function(el) {
+    var str = false; 
+    if(el.getAttribute('JGBind')) { 
+      str = el.getAttribute('JGBind').split('.'); 
+      var val = el.innerHTML; 
+    } else if (el.getAttribute('JGValue')) { 
+      str = el.getAttribute('JGValue').split('.'); 
+      var val = el.value; 
+    }
+    if(str) { 
+      var objName = str.shift(); 
+      str = str.join('.'); 
+      // only fire if the object prop has changed: 
+      if(get(state.objects[objName], str) != val) { 
+        set( state.objects[objName], str, val ); 
+        state.queue.push({
+          objName: objName, 
+          obj: state.objects[objName], 
+          changeinitiator: el
+        }); 
+        if(state.domCollected && !state.processingQueue) { 
+          processQueue(); 
+        }
+        // check for changeHandler functions tied to this key or $base {obj}
+        if(typeof state.changeHandlers[objName] != "undefined") { 
+          if(typeof state.changeHandlers[objName][str] != "undefined") { 
+            state.changeHandlers[objName][str](); 
+          } 
+          if(typeof state.changeHandlers[objName]['$base'] != "undefined") { 
+            // if the handler is tied to base function then we pass the keyStr: 
+            state.changeHandlers[objName]['$base'](str); 
+          } 
+        }
+      }
+    }
+  }; 
+
+  var processQueue = function() { 
+    var len = state.queue.length; 
+    for(var i=0; i < len; ++i) { 
+      executeBind(state.queue[i]); 
+    } 
+    state.queue.splice(0,len); 
+    if(state.queue.length > 0) { 
+      processQueue(); 
+    }
+  }; 
+
+  var executeBind = function(task) { 
+    var arr = state.boundelems[task.objName]; 
+    for(var i=0; i < arr.length; ++i) { 
+      // don't update the element that invoked the change to prevent cursor problems: 
+      if(arr[i].el != task.changeinitiator) { 
+        if(arr[i].type=='bind') { 
+          arr[i].el.innerHTML = get(task.obj, arr[i].key); 
+        } else if(arr[i].type=='value') { 
+          arr[i].el.setAttribute('value', get(task.obj, arr[i].key)); 
+          arr[i].el.value = get(task.obj, arr[i].key); 
+        } 
+        // if(arr[i].type=='show') { 
+        //   console.log(task.objName); 
+        //   console.log(arr[i]); 
+        // }
+        // if(arr[i].type=='show') { 
+        //   var val = get(task.obj,arr[i].key); 
+        //   if(val != '' && val != false && val != null && val.toString().toLowerCase() != 'false') { 
+        //     arr[i].el.style.display = ""; 
+        //   } else { 
+        //     arr[i].el.style.display = "none"; 
+        //   } 
+        // }
+        // if(arr[i].type=='hide') { 
+        //   if(val != '' && val != false && val != null && val.toString().toLowerCase() != 'false') { 
+        //     arr[i].el.style.display = "none"; 
+        //   }
+        // } 
+      } 
+    } 
+  }; 
+
+  var uploadObject = function(objName) { 
+    for(var i=0; i < state.boundelems[objName].length; ++i) { 
+      if(state.boundelems[objName][i]['type'] == 'bind') { 
+        set(state.objects[objName],state.boundelems[objName][i]['key'],state.boundelems[objName][i]['el'].innerHTML); 
+      } else if (state.boundelems[objName][i]['type'] == 'value') { 
+        set(state.objects[objName],state.boundelems[objName][i]['key'],state.boundelems[objName][i]['el'].getAttribute('value')); 
+      }
+    } 
+    return state.objects[objName]
+  }; 
+
+  var set = function(obj,str,val) { 
+    str = str.split('.'); 
+    while(str.length > 1) { 
+      obj = obj[str.shift()]; 
+    }
+    return obj[str.shift()] = val; 
+  };
+
+  var get = function(obj,str) { 
+    str = str.split('.'); 
+    for(var i=0; i < str.length; ++i) { 
+      if(typeof obj[str[i]] == 'undefined') { 
+        return ''; 
+      } else { 
+        obj = obj[str[i]]; 
+      }
+    } 
+    return obj; 
+  }; 
+
+  var binder = function(objName,obj) { 
+    // state.originalObjects[objName] = JSON.parse(JSON.stringify(obj)); 
+    state.originalObjects[objName] = $.extend({},obj);
+    state.objects[objName] = obj; 
+
+    state.queue.push({
+      objName: objName, 
+      obj: obj, 
+      changeinitiator: false
+    }); 
+
+    if(state.domCollected && !state.processingQueue) { 
+      processQueue(); 
+    }
+  }; 
+
+  var public = { 
+    // ex: JGBinder.bind('strain', {article: '', title: ''}); 
+    set: function(objName, obj) {
+      binder(objName,obj); 
+    },
+    // ex: var fetchedObj = JGBinder.fetch('strain'); 
+    fetch: function(objName) {
+      return uploadObject(objName);  
+    }, 
+    // ex: JGBinder.revert('strain'); 
+    revert: function(objName) { 
+      binder(objName, state.originalObjects[objName]); 
+    },
+    // ex: JGBinder.onChange('strain',function('propName',newValue,){}); 
+    onChange: function(objName,propName,fn) { 
+      if(typeof state.changeHandlers[objName] == 'undefined') { 
+        state.changeHandlers[objName] = { }; 
+      } 
+      if(typeof fn == "function") { 
+        state.changeHandlers[objName][propName] = fn; 
+      }
+    }, 
+    reload: function() { 
+      collectDOM(); 
+      return true; 
+    }
+  };  
+
+  $.ready(collectDOM); 
+
+  return { 
+    set: public.set, 
+    fetch: public.fetch, 
+    revert: public.revert, 
+    onChange: public.onChange, 
+    reload: public.reload
+  } 
+
+})(window,document,console,jDom); 
--- build/js/time.js
+++ build/js/time.js
@@ -1,11 +1,113 @@
-var time = (function(w,d,c,exports){
-	/* This function will be used for all the time things: */
-	var today, getMonth, getDaysInMonth, getWeeksInMonth, daysInMonths, buildMonth, padMonth, labels = {};
+var calendar = (function(w,d,c,D,$,exports){
+	// variables: 
+	var today, basedays, basedayskeys, dayspermonth, 
+			labels = {}, 
+			state = { count: 0, calendars: {} };
+
+	today = new D(); 
+	basedays = {}; 
+	basedayskeys = []; 
+	dayspermonth = [31,28,31,30,31,30,31,31,30,31,30,31]; 
 	labels.months = ["January","February","March","April","May","June","July","August","September","October","November","December"];  
 	labels.days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
-	daysInMonths = [31,28,31,30,31,30,31,31,3031,30,31]; 
+	// setup our basedays templates: 
+	for(var i=0; i < 35; ++i) { 
+		basedayskeys.push('day_'+(i+1)); 
+		basedays['day_'+(i+1)] = {}; 
+	}
 
-	today = new Date(); 
+	// functions: 
+	var generateMonth, getMonth, setMonth, padMonth, getDaysInMonth, getWeeksInMonth, applyDaysTo, addMonth, subtractMonth;
+	addMonth = function() { 
+		var id, month, year; 
+		id = this.getAttribute('data-jcalid'); 
+		if(state.calendars[id].month == 12) { 
+			year = 1 + state.calendars[id].year; 
+			month = 1;
+		} else { 
+			year = state.calendars[id].year; 
+			month = 1 + state.calendars[id].month; 
+		}
+		setMonth(id,month,year); 
+	}; 
+	subtractMonth = function() { 
+		var month, year, id = this.getAttribute('data-jcalid'); 
+		if(state.calendars[id].month == 1) { 
+			month = 12; 
+			year = state.calendars[id].year - 1; 
+		} else { 
+			month = state.calendars[id].month -1; 
+			year = state.calendars[id].year; 
+		}
+		setMonth(id,month,year); 
+	};  
+	generateMonth = function(elem) { 
+		var calendar = {}, contains = [], id = 'calendar_' + state.count; 
+		calendar.type = 'div'; 
+		calendar.attributes = { 'class': 'jCal', id: id };
+		calendar.contains = []; 
+		calendar.contains.push({
+			type: 'div', 
+			attributes: {'class': 'row jCalHeader'}, 
+			contains: [
+				{
+					type: 'a', 
+					attributes: { 'class': 'jCalBtn col left', 'data-jcalid': id },
+					bindings: { 'click': subtractMonth },
+					contains: [ '<' ]
+				}, { 
+					type: 'div', 
+					attributes: { 'class': 'jCalLabel col', 'data-jcalid': id }, 
+					contains: [ 
+						{ 
+							type: 'span', 
+							attributes: { 'class': 'month', 'JGBind': id+'.monthLabelAbbr' }
+						}, { 
+							type: 'span', 
+							attributes: { 'class': 'year', 'JGBind': id+'.year' }
+						}
+					] 
+				}, {
+					type: 'a', 
+					attributes: { 'class': 'jCalBtn col left', 'data-jcalid': id },
+					bindings: { 'click': addMonth },
+					contains: [ '>' ]
+				}
+			]
+		}); 
+		for(var i = 0; i < 5; ++i) { 
+			var row = { type: 'a', attributes: { 'class': 'row jCalRow' }, contains: []}; 
+			for(var j = 0; j < 7; ++j) { 
+				row.contains.push({
+					type: 'div', 
+					attributes: { 'class': 'col jCalCell' }, 
+					contains: [ 
+						{ 
+							type: 'img', 
+							attributes: { src: 'dist/img/square.png', 'class': 'squaringImage', width: '100%', height: 'auto' }
+						}, { 
+							type: 'div', 
+							attributes: { 'class': 'innerCell' }, 
+							contains: [
+								{ 
+									type: 'span', 
+									attributes: { 
+										'JGBind': id+'.days.day_'+(((i*7) + j+1)+'.day') 
+									}
+								}
+							]
+						}
+					]
+				})
+			}
+			calendar.contains.push(row); 
+		}
+		state.calendars[id] = { }; 
+		state.calendars[id].el = elem.appendChild($.create(calendar)); 
+		state.calendars[id].days = $.extend({},basedays); 
+		++state.count;  
+		return id; 
+	}; 
 
 	padMonth = function(days,padding) { 
 		for(var i=0; i < padding[0]; ++i) { 
@@ -17,65 +119,64 @@ var time = (function(w,d,c,exports){
 		return days; 
 	}; 
 
-	buildMonth = function(days) { 
-		var numWeeks, padding = [], html = {}; 
-		numWeeks = getWeeksInMonth(days); 
-		padding[0] = days[0].getDay(); 
-		padding[1] = numWeeks*7 - days.length - padding[0]; 
-		days = padMonth(days,padding); 
-		html.type = 'div'; 
-		html.attributes = { 'class': 'jCal' };
-		html.contains = [];  
-		for(var i=0; i < numWeeks; ++i) { 
-			var row = {type: 'div', attributes: { 'class': 'row jCalCell' }, contains: []}; 
-			for(var j=0; j < 7; ++j) { 
-				var thisday = days[i*7+j]; 
-				row.contains.push({
-					type: 'div',
-					attributes: { 'class': (thisday) ? '' : 'empty' }, 
-					contains: (thisday) ? thisday.getDate().toString() : '' 
-				})
-				c.log(row);
-			}
-			html.contains.push(row); 
-		}
-		c.log(jDom.create(html)); 
-	}; 
-	getWeeksInMonth = function(days) { 
-		c.log("DAYS IN MONTH: " + days.length); 
-		var firstWeek; 
-		firstWeek = 7 - days[0].getDay(); 
-		return Math.ceil((days.length-firstWeek)/7 + 1); 
-	}; 
 	getDaysInMonth = function(month,year) { 
 		if(month == 1 && (year%4===0 && (year%100!=0 || year%400!=0))) { 
 			return 29; 
 		} else { 
-			return daysInMonths[month]; 
+			return dayspermonth[month]; 
 		}
 	}; 
+
 	getMonth = function(month,year) { 
 		var numDays, days = [];  
 		numDays = getDaysInMonth(month,year); 
 		for(var i=0; i < numDays; ++i) { 
 			days.push(new Date(year,month,i+1)); 
 		}	
-		buildMonth(days); 
+		c.log(numDays); 
 		return days;  
 	}; 
-	exports.getMonth = function(month,year) { 
+
+	applyDaysTo = function(id,days) { 
+		var startingNull = 0; 
+		for(var i=0; i < days.length; ++i) { 
+			startingNull += (days[i]) ? 0 : 1; 
+			state.calendars[id].days['day_'+(i+1)] = { 
+				day: (days[i]) ? i+1-startingNull : days[i], 
+				date: days[i]
+			}; 
+		}
+		JGBinder.reload(); 
+		JGBinder.set(id,state.calendars[id]) 
+	}; 
+	
+	setMonth = function(id,month,year) { 
 		month = (typeof month == 'undefined') ? today.getMonth() : month-1; 
 		year = (typeof year == 'undefined') ? today.getYear() : year; 
-		var days; 
-		days = getMonth(month,year); 	
-		return days; 
-	};
-	exports.getWeek = function(start) { 
-
+		var days, numWeeks, padding = []; 
+		days = getMonth(month,year);
+		padding[0] = days[0].getDay(); 
+		padding[1] = 35 - days.length - padding[0]; 
+		days = padMonth(days,padding); 
+		state.calendars[id].month = month+1; 
+		state.calendars[id].monthLabel = labels.months[month]; 
+		state.calendars[id].monthLabelAbbr = labels.months[month].substring(0,3); 
+		state.calendars[id].year = year; 
+		applyDaysTo(id,days);
 	}; 
-	exports.getDay = function(date) { 
 
-	};  
+	exports.generateCalendar = function(id,month,year) { 
+		var calendarId = generateMonth(d.getElementById(id)); 
+		setMonth(calendarId,month,year); 		 
+		return calendarId; 	
+	}
+
+	exports.setMonth = function(id,month,year) { 
+		if(typeof state.calendars[id] != 'undefined') { 
+			setMonth(id,month,year); 
+		}
+	}; 
 
 	return exports; 
-})(window,document,console,time||{}); 
+
+})(window,document,console,Date,jDom,calendar||{}); 
